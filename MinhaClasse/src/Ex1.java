f6dc2cba2f20aea3c11c5469b671b861aa800003
Yuri Budke <yuribudke2006@gmail.com>
2026-02-19 21:29:46 -0300

    Adiciona Ex1

---
+++ MinhaClasse/src/Ex1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Informe sua matrícula: ");
+		String mat = scanner.next();
+		
+		System.out.println("Informe seu nome: ");
+		String nome = scanner.next();
+		
+		System.out.println("Informe a quantidade de horas que você trabalhou: ");
+		int hr = scanner.nextInt();
+		
+		System.out.println("Informe o valor que você recebe por hora: ");
+		double valor = scanner.nextDouble();
+		
+		double salmen = (hr * valor) * 28;
+		
+		System.out.println("\nMatrícula: " + mat + "\nNome: " + nome + "\nSalário: " + salmen);
+	
+		scanner.close();
+
+	}
+
+}
